1727b88de44b42542b524629877443a586e6a638
Andy <andypsil0n@gmx.de>
2023-01-26 11:34:35 +0100

    minor Changes to Menu Bar

--- frontend/frifoo_flutter_frontend/lib/pages/navpages/home_page.dart
+++ frontend/frifoo_flutter_frontend/lib/pages/navpages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frifoo_flutter_frontend/customWidgets/ImageBox.dart';
 import 'package:frifoo_flutter_frontend/customWidgets/MainButton.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,7 +12,12 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             Text("Home"),
-            MainButton(inputText: "Hi")
+            MainButton(inputText: "Hi"),
+            ImageBox(
+              height: 300,
+              width: 400,
+              imageSource: 'https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            ),
           ],
         ),
       ),
