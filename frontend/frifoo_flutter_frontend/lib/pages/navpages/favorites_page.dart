d5727f891a82cc9a424a67ca969ddef94dfa2024
Begüm Nur AK <begumnurak@gmail.com>
2023-01-31 10:44:22 +0100

    first layout version for the page "favourites"

--- frontend/frifoo_flutter_frontend/lib/pages/navpages/favorites_page.dart
+++ frontend/frifoo_flutter_frontend/lib/pages/navpages/favorites_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:frifoo_flutter_frontend/customWidgets/ImageBox.dart';
+import 'package:frifoo_flutter_frontend/customWidgets/MainButton.dart';
 
 class FavoritesPage extends StatelessWidget {
   const FavoritesPage({super.key});
@@ -7,8 +9,17 @@ class FavoritesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text(
-          "Favorites"
+        child: Column(
+          children: [
+            Text("Favourites"),
+            MainButton(inputText: "Beispieltext"),
+            ImageBox(
+              height: 300,
+              width: 400,
+              imageSource:
+                  'https://cdn.kisikates.com.tr/image-cache/cache/recipe_main_image_large/https---cdn.kisikates.com.tr/recipe-media/8d35632c3965b0e910325bd6ab9a43996d32b61e.jpeg',
+            ),
+          ],
         ),
       ),
     );
