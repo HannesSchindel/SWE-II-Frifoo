1f12cee0a691427fae46d66265b141e0f2ec6949
Andy Ringelmann <->
2023-02-24 13:07:30 +0100

    Added Horizontal List Views on HomePage

--- frontend/frifoo_flutter_frontend/lib/pages/navpages/home_page.dart
+++ frontend/frifoo_flutter_frontend/lib/pages/navpages/home_page.dart
@@ -21,10 +21,61 @@ class HomePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text('Featured', style: FONT_FIRST_HEADING),
-                  Text('Hallo Hallo Wie Geht es dir?', style: FONT_PARAGRAPH)
+                  Text('Hallo Hallo Wie Geht es dir?', style: FONT_PARAGRAPH),
+                  Container(
+                    height: 200,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 4,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.only(right: 25),
+                          height: 200,
+                          width: 150,
+                          child: Center(
+                            child: ImageBox(
+                              height: 200,
+                              width: 150,
+                              imageSource: 'https://images.pexels.com/photos/2664443/pexels-photo-2664443.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',)
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 ],
               ),
-            )
+            ),
+            Container(
+              margin: const EdgeInsets.all(MAIN_CONTAINER_MARGIN),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Favorites', style: FONT_FIRST_HEADING),
+                  Text('Hallo Hallo Wie Geht es dir?', style: FONT_PARAGRAPH),
+                  Container(
+                    height: 200,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 4,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.only(right: 25),
+                          height: 200,
+                          width: 150,
+                          child: Center(
+                            child: ImageBox(
+                              height: 200,
+                              width: 150,
+                              imageSource: 'https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',)
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            
           ],
         ),
     );
