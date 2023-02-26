844c69a666fb6df99da7d63ca384806bb9bd46c4
Begüm Nur AK <begumnurak@gmail.com>
2023-02-26 21:29:31 +0100

    added heart-shaped icon and subheadings to the recipes

--- frontend/frifoo_flutter_frontend/lib/pages/navpages/favorites_page.dart
+++ frontend/frifoo_flutter_frontend/lib/pages/navpages/favorites_page.dart
@@ -5,47 +5,100 @@ import 'package:frifoo_flutter_frontend/constants.dart';
 import 'package:frifoo_flutter_frontend/customWidgets/ImageBox.dart';
 import 'package:frifoo_flutter_frontend/customWidgets/MainButton.dart';
 
-class FavoritesPage extends StatelessWidget {
+class FavoritesPage extends StatefulWidget {
   const FavoritesPage({super.key});
+
+  @override
+  _FavoritesPageState createState() => _FavoritesPageState();
+}
+
+class _FavoritesPageState extends State<FavoritesPage> {
+  List<bool> _isFavoriteList = List.generate(20, (index) => false);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 15.0, left: 16.0),
-          child: Text(
-            "FAVOURITES",
-            style: FONT_FIRST_HEADING.copyWith(fontSize: 24.0),
-            textAlign: TextAlign.left,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 15.0, left: 16.0),
+            child: Text(
+              "FAVORITES",
+              style: FONT_FIRST_HEADING.copyWith(fontSize: 24.0),
+              textAlign: TextAlign.left,
+            ),
           ),
-        ),
-        Expanded(
+          Expanded(
             child: GridView.count(
-                padding: EdgeInsets.only(top: 50.0),
-                crossAxisCount: 2,
-                childAspectRatio: 1.0,
-                children: List.generate(
-                  20,
-                  (index) {
-                    return Center(
-                      child: Stack(
-                        children: [
-                          ImageBox(
-                            width: 200,
-                            height: 250,
-                            imageSource:
-                                'https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg',
+              padding: EdgeInsets.only(top: 50.0),
+              crossAxisCount: 2,
+              childAspectRatio: 1.0,
+              children: List.generate(
+                20,
+                (index) {
+                  return Center(
+                    child: Stack(
+                      children: [
+                        ImageBox(
+                          //title: "Rezept",
+                          width: 200,
+                          height: 250,
+                          //isFavourite: true,
+                          imageSource:
+                              'https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg',
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          left: 0,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 22.0, bottom: 20.0),
+                                child: Text(
+                                  "Rezept",
+                                  style: FONT_FIRST_HEADING.copyWith(
+                                      fontSize: 14.0),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                    );
-                  },
-                )))
-      ],
-    ));
+                        ),
+                        Positioned(
+                          top: 15,
+                          right: 10,
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _isFavoriteList[index] =
+                                    !_isFavoriteList[index];
+                              });
+                            },
+                            child: Container(
+                              child: Icon(
+                                Icons.favorite,
+                                // border:
+                                //     Border.all(color: Colors.red, width: 2.0),
+                                color: _isFavoriteList[index]
+                                    ? Colors.transparent
+                                    : Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
