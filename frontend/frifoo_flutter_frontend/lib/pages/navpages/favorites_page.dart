f12dbf23340627baa7b37f90fbc6cb085e6b6cc3
Begüm Nur AK <begumnurak@gmail.com>
2023-02-26 20:21:14 +0100

    added a resizeable, left-aligned headline and adjusted the images

--- frontend/frifoo_flutter_frontend/lib/pages/navpages/favorites_page.dart
+++ frontend/frifoo_flutter_frontend/lib/pages/navpages/favorites_page.dart
@@ -1,3 +1,4 @@
+import 'dart:html';
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:frifoo_flutter_frontend/constants.dart';
@@ -10,39 +11,82 @@ class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(20, (index) {
-          return Center(
-            child: ImageBox(
-              height: 200,
-              width: 200,
-              imageSource:
-                  'https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg',
-            ),
-          );
-        }),
-      ),
-      // child: Center(
-      //   child: Column( //
-      //     crossAxisAlignment: CrossAxisAlignment.start, // Inhalt linksbündig
-      //     children: [
-      //       Padding(
-      //         padding: const EdgeInsets.only(top: 15.0),
-      //         child: Text(
-      //           "FAVOURITES",
-      //           style: FONT_FIRST_HEADING,
-      //         ),
-      //       ),
-      //       // MainButton(inputText: "Beispieltext"),
-      //       ImageBox(
-      //         height: 300,
-      //         width: 400,
-      //         imageSource:
-      //             'https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg',
-      //       ),
-      //     ],
-      //   ),
-    );
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 15.0, left: 16.0),
+          child: Text(
+            "FAVOURITES",
+            style: FONT_FIRST_HEADING.copyWith(fontSize: 24.0),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Expanded(
+            child: GridView.count(
+                padding: EdgeInsets.only(top: 50.0),
+                crossAxisCount: 2,
+                childAspectRatio: 1.0,
+                children: List.generate(
+                  20,
+                  (index) {
+                    return Center(
+                      child: Stack(
+                        children: [
+                          ImageBox(
+                            width: 200,
+                            height: 250,
+                            imageSource:
+                                'https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg',
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                )))
+      ],
+    ));
   }
 }
+
+
+
+
+//     return Container(
+//       child: GridView.count(
+//         crossAxisCount: 2,
+//         childAspectRatio: 1.1, // Seitenverhältnis der Widgets bzw. Bilder
+//         children: List.generate(20, (index) {
+//           return Center(
+//             child: ImageBox(
+//               height: 200, // Höhe der Widgets bzw. Bilder
+//               width: 200, // Breite der Widgets bzw. Bilder
+//               imageSource:
+//                   'https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg',
+//             ),
+//           );
+//         }),
+//       ),
+//       // child: Center(
+//       //   child: Column( //
+//       //     crossAxisAlignment: CrossAxisAlignment.start, // Inhalt linksbündig
+//       //     children: [
+//       //       Padding(
+//       //         padding: const EdgeInsets.only(top: 15.0),
+//       //         child: Text(
+//       //           "FAVOURITES",
+//       //           style: FONT_FIRST_HEADING,
+//       //         ),
+//       //       ),
+//       //       // MainButton(inputText: "Beispieltext"),
+//       //       ImageBox(
+//       //         height: 300,
+//       //         width: 400,
+//       //         imageSource:
+//       //             'https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg',
+//       //       ),
+//       //     ],
+//       //   ),
+//     );
+//   }
+// }
