21297ac84df3756d6e5a6afacc25def024b8cd4f
HannesSchindel <hannesiphone18@icloud.com>
2023-02-28 13:47:04 +0100

    chartbox rogtyp erstellt

---
+++ frontend/frifoo_flutter_frontend/lib/customWidgets/ChartBox.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:frifoo_flutter_frontend/constants.dart';
+
+class ChartBox extends StatelessWidget {
+  final double _borderRadius = 30.0;
+  final double width;
+  final double height;
+
+  const ChartBox({required this.width, required this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(_borderRadius),
+        color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 10,
+              offset: Offset(4, 4),
+           ),
+         ],
+      ),
+      child:
+    );
+  }
+}
+
+/*
+
+   
+*/
