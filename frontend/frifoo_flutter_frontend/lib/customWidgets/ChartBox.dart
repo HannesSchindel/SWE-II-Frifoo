97fdb7fb3c715e5108b672549af6f5b20871a302
HannesSchindel <hannesiphone18@icloud.com>
2023-02-28 17:36:01 +0100

    sicherheits commmit

--- frontend/frifoo_flutter_frontend/lib/customWidgets/ChartBox.dart
+++ frontend/frifoo_flutter_frontend/lib/customWidgets/ChartBox.dart
@@ -1,5 +1,8 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:frifoo_flutter_frontend/constants.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class ChartBox extends StatelessWidget {
   final double _borderRadius = 30.0;
@@ -11,21 +14,23 @@ class ChartBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: width,
-      height: height,
-      margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(_borderRadius),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey,
-            blurRadius: 10,
-            offset: Offset(4, 4),
-          ),
-        ],
-      ),
-    );
+        width: width,
+        height: height,
+        margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(_borderRadius),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 10,
+              offset: Offset(4, 4),
+            ),
+          ],
+        ),
+        child: Scaffold(
+          body: SfCartesianChart(),
+        ));
   }
 }
 
