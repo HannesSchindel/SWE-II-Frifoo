8b7909b62f8da6d2dcf5aab62a35e741a3790df1
HannesSchindel <hannesiphone18@icloud.com>
2023-02-28 13:33:59 +0100

    funktionierende Zwischenversion

--- frontend/frifoo_flutter_frontend/lib/pages/navpages/statistics_page.dart
+++ frontend/frifoo_flutter_frontend/lib/pages/navpages/statistics_page.dart
@@ -18,31 +18,11 @@ class StatisticsPage extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Featured', style: FONT_FIRST_HEADING),
-                Text('Check out these featured Recipes', style: FONT_PARAGRAPH),
-                Container(
-                  height: 200,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 4,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        margin: EdgeInsets.only(right: 25),
-                        height: 200,
-                        width: 150,
-                        child: Center(
-                            child: ImageBox(
-                          title: "Rezept $index",
-                          height: 200,
-                          width: 150,
-                          isFavorite: false,
-                          imageSource:
-                              'https://images.pexels.com/photos/2641886/pexels-photo-2641886.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-                        )),
-                      );
-                    },
-                  ),
-                ),
+                Text('Statistics', style: FONT_FIRST_HEADING),
+                Text(
+                    'Here you have an overview of all the nutritional values and calories of the last dishes.',
+                    style: FONT_PARAGRAPH),
+                Container(),
               ],
             ),
           ),
