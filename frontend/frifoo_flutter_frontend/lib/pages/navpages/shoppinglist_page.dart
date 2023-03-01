01badb20c1c6f9acd2db049d9d5a2f395a1ad5e9
Luca Ziehe <lucaziehe2003@gmail.com>
2023-03-01 10:05:42 +0100

    shopping

--- frontend/frifoo_flutter_frontend/lib/pages/navpages/shoppinglist_page.dart
+++ frontend/frifoo_flutter_frontend/lib/pages/navpages/shoppinglist_page.dart
@@ -1,16 +1,65 @@
 import 'package:flutter/material.dart';
+import '../../constants.dart';
+import '../../customWidgets/ImageBox.dart';
+import '../../customWidgets/MainFloatingButton.dart';
 
-class ShoppingListPage extends StatelessWidget {
+class ShoppingListPage extends StatefulWidget {
   const ShoppingListPage({super.key});
+
+  @override
+  State<ShoppingListPage> createState() => _ShoppingListPageState();
+}
+
+class _ShoppingListPageState extends State<ShoppingListPage> {
+  List<bool> _ShoppingList = List.generate(20, (index) => false);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text(
-          "Shopping List"
-        ),
+      margin: const EdgeInsets.all(MAIN_CONTAINER_MARGIN),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Shopping List", style: FONT_LOGO_BLACK_HEADING),
+          Expanded(
+            child: GridView.count(
+              padding: EdgeInsets.only(top: 50.0),
+              crossAxisCount: 2,
+              childAspectRatio: 1.0,
+              children: List.generate(
+                20,
+                (index) {
+                  return Center(
+                    child: Stack(
+                      children: [
+                        ImageBox(
+                          title: "Item $index",
+                          height: 200,
+                          width: 150,
+                          isFavorite: false,
+                          imageSource:
+                              'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1280px-Oranges_-_whole-halved-segment.jpg',
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+          Positioned(
+              right: 500,
+              child:
+                  MainFloatingButton(icon: Icon(Icons.add), onPressed: () {})),
+        ],
       ),
     );
   }
 }
+
+
+// IconButton(
+//                             icon: Image.asset(''),
+//                             iconSize: 50,
+//                             onPressed: () {},
+//                           )
